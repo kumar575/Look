0de31ec57ba1f19dfbae149bcacb0dcb6cf0e8ef
kumarutsav7 <kumar.u@vectone.com>
2020-09-01 02:01:54 +0530

    Local changes for reversing the digit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,3 +24,78 @@ class Solution {
          return reversed;
     }
 }
+
+class Solutions {
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+
+        double result = 0;
+
+        // remove whitespaces
+
+        str = str.trim();
+        // handle positive and negative sign
+        boolean isNegative = false;
+        int startIndex = 0;
+
+        if (str.charAt(0) == '+' || str.charAt(0) == '-') {
+            startIndex++;
+        }
+        if (str.charAt(0) == '-') {
+            isNegative = true;
+        }
+
+        for(int i = startIndex; i < str.length(); i++) {
+            if (str.charAt(i) < '0' || str.charAt(i) > '9'){
+                break;
+            }
+            int digitValue = (int)(str.charAt(i) - '0');
+            result = result * 10 + digitValue;
+        }
+
+        if(isNegative) {
+            result = -result;
+        }
+
+        // handle out of integer range
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int)result;
+
+    }
+}
+
+class Solution_Palindrome {
+    public boolean isPalindrome(int x) {
+// A number that remains the same when its digits are reversed.
+        if (x < 0 || x != 0 && x % 10 == 0) {
+            return false ;
+        }
+
+        // reverse process
+        int reversed = 0;
+        int original = x;
+        while( x != 0){
+            reversed = reversed * 10 + x % 10;
+            x = x/10;
+        }
+
+
+
+
+        return reversed == original;
+    }
+}
+
+class Solution_Regular {
+    public boolean isMatch(String s, String p) {
+      return true;
+    }
+}
