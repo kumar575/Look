3ed6feb9418f601135040b460c14f3b9897e294a
kumarutsav7 <kumar.u@vectone.com>
2020-09-01 01:31:22 +0530

    Local changes for reversing the digit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("hot loca is not happy");
+    }
+}
+
+
+class Solution {
+    public int reverse(int x) {
+         int reversed = 0;
+         // 123
+         while(x != 0) {
+           int  temp = reversed * 10 + x % 10; // 3 * 10 + 2
+             if (temp / 10 != reversed ) {
+                 return 0;
+             }
+             reversed = temp;  // 3
+             x = x / 10; // 12
+         }
+
+         return reversed;
+    }
+}
