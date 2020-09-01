1829bfe086486f725bca138c2cb881870b7b7366
kumarutsav7 <kumar.u@vectone.com>
2020-09-01 17:29:36 +0530

    Local changes for reversing the digit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -157,3 +157,26 @@ class Solution_water {
 
     }
 }
+
+class Solution_Roman {
+    public String intToRoman(int num) {
+
+        int[] arabics = new int[] {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romans = new String[] {"M", "CM", "D", "CD", "C", "XC", "L", "XL","X","IX", "V", "IV", "I" };
+
+//        StringBuilder sb = new StringBuilder();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < arabics.length ; i++) {
+            while(num - arabics[i] >= 0){
+                sb.append(romans[i]);
+                num = num - arabics[i];
+            }
+        }
+        return sb.toString();
+
+
+
+    }
+}
