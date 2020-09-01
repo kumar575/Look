13149350225414f1be81274a3917e63494c810df
kumarutsav7 <kumar.u@vectone.com>
2020-09-01 13:37:57 +0530

    Local changes for reversing the digit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -51,7 +51,7 @@ class Solutions {
             if (str.charAt(i) < '0' || str.charAt(i) > '9'){
                 break;
             }
-            int digitValue = (int)(str.charAt(i) - '0');
+            int digitValue = str.charAt(i) - '0';
             result = result * 10 + digitValue;
         }
 
@@ -96,6 +96,64 @@ class Solution_Palindrome {
 
 class Solution_Regular {
     public boolean isMatch(String s, String p) {
-      return true;
+
+        boolean [][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        char[] string = new char[s.length() + 1];
+        char[] pattern = new char[p.length() + 1];
+
+        string[0] = (char)0;
+        pattern[0] = (char)0;
+
+        for(int i = 0; i < s.length(); i++) {
+            string[i + 1] = s.charAt(i);
+        }
+
+        for(int i = 0; i < p.length(); i++) {
+            pattern[i + 1] = s.charAt(i);
+        }
+
+        for(int i = 0; i < p.length(); i++) {
+            if (pattern[i] == '*' && dp[0][i - 2]) {
+                dp[0][i] = true;
+            }
+        }
+
+        for(int i = 1 ; i < s.length(); i++) {
+            for(int j = 1; j < pattern.length; j++) {
+                if(pattern[j] == string[i] || pattern[j] == '.') {
+                    dp[i][j] = dp[i -1][j -1];
+                } else if(pattern[i] == '*') {
+                    if(pattern[j-1] == string[i] || pattern[j - 1] == '.'){
+                        dp[i][j] = dp[i][j-2] || dp[i-1][j];
+                    }else {
+                        dp[i][j] =  dp[i ][j - 2];
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+}
+
+class Solution_water {
+    public int maxArea(int[] height) {
+
+      int max_area = 0;
+      int a_pointer = 0;
+      int b_pointer = height.length - 1;
+
+      while(a_pointer < b_pointer) {
+          if (height[a_pointer] < height[b_pointer]) {
+              max_area = Math.max(max_area, height[a_pointer] * (b_pointer - a_pointer));
+              a_pointer += 1;
+          }else {
+              max_area = Math.max(max_area, height[b_pointer] * (b_pointer - a_pointer));
+              b_pointer -= 1;
+          }
+      }
+      return max_area;
+
     }
 }
